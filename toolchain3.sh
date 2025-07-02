af16f8292ef9f94642ee59f852aa1705389cd2d0
Chase <chase@unknownde00a889b074.attlocal.net>
2025-07-02 11:10:37 -0500

    fixing gcc build error

--- toolchain3.sh
+++ toolchain3.sh
@@ -30,25 +30,25 @@ mkdir -v build
 cd build
 
 # Configure the build
-../configure \\
-    --target=\$LFS_TGT \\
-    --prefix=\$LFS/tools \\
-    --with-glibc-version=2.41 \\
-    --with-sysroot=\$LFS \\
-    --with-newlib \\
-    --without-headers \\
-    --enable-default-pie \\
-    --enable-default-ssp \\
-    --disable-nls \\
-    --disable-shared \\
-    --disable-multilib \\
-    --disable-threads \\
-    --disable-libatomic \\
-    --disable-libgomp \\
-    --disable-libquadmath \\
-    --disable-libssp \\
-    --disable-libvtv \\
-    --disable-libstdcxx \\
+../configure \
+    --target=\$LFS_TGT \
+    --prefix=\$LFS/tools \
+    --with-glibc-version=2.41 \
+    --with-sysroot=\$LFS \
+    --with-newlib \
+    --without-headers \
+    --enable-default-pie \
+    --enable-default-ssp \
+    --disable-nls \
+    --disable-shared \
+    --disable-multilib \
+    --disable-threads \
+    --disable-libatomic \
+    --disable-libgomp \
+    --disable-libquadmath \
+    --disable-libssp \
+    --disable-libvtv \
+    --disable-libstdcxx \
     --enable-languages=c,c++
 
 make
@@ -56,8 +56,8 @@ make install
 
 # Create the limits.h header
 cd ..
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \\
-  \`dirname \$(\${LFS_TGT}-gcc -print-libgcc-file-name)\`/include/limits.h
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  \`dirname \$(${LFS_TGT}-gcc -print-libgcc-file-name)\`/include/limits.h
 
 EOF
 
