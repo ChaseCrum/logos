5186e319ab724974a9c282c13ae6a3ea66a33b04
Chase <chase@unknownde00a889b074.attlocal.net>
2025-07-04 16:50:54 -0500

    fixing temptools for chapter 6

--- temptools.sh
+++ temptools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Fix LFS variable if running with sudo
+# === FIX: Preserve LFS variable even under sudo ===
 if [ -z "$LFS" ]; then
   if [ -n "$SUDO_USER" ]; then
-    eval LFS=$(sudo -u "$SUDO_USER" env | grep '^LFS=' | cut -d= -f2-)
+    LFS=$(sudo -u "$SUDO_USER" bash -c 'echo $LFS')
     export LFS
   fi
 fi
@@ -14,70 +14,83 @@ if [ -z "$LFS" ]; then
   exit 1
 fi
 
-# Run script as 'lfs' user if executed by root
+# === If run as root, switch to 'lfs' user ===
 if [ "$(id -u)" -eq 0 ] && [ "$USER" != "lfs" ]; then
   echo "✅ Running as root. Executing build steps as 'lfs' user..."
-  sudo -u lfs LFS=$LFS bash "$0"
-  exit $?
+  exec sudo -H -u lfs env LFS="$LFS" bash "$0"
 fi
 
 cd $LFS/sources
 
-# M4
-tar -xf m4-*.tar.* && cd m4-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf m4-*/
+build_and_install() {
+  PACKAGE_GLOB="$1"
+  CONFIGURE_CMD="$2"
+  MAKE_FLAGS="$3"
+  POST_INSTALL="$4"
 
-# Ncurses
-tar -xf ncurses-*.tar.* && cd ncurses-*/
+  SRC_DIR=$(find . -type d -name "$PACKAGE_GLOB*" -print -quit)
+  if [ ! -d "$SRC_DIR" ]; then
+    echo "❌ Source directory not found for $PACKAGE_GLOB"
+    exit 1
+  fi
+
+  echo "🔧 Building $PACKAGE_GLOB in $SRC_DIR"
+  cd "$SRC_DIR"
+
+  if [ -f configure ]; then
+    eval "$CONFIGURE_CMD"
+  fi
+
+  eval "make $MAKE_FLAGS"
+  make DESTDIR=$LFS install
+
+  if [ -n "$POST_INSTALL" ]; then
+    eval "$POST_INSTALL"
+  fi
+
+  cd $LFS/sources
+  echo "✅ $PACKAGE_GLOB build complete."
+}
+
+# 6.2 M4
+build_and_install "m4-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(build-aux/config.guess)" "" ""
+
+# 6.3 Ncurses
+NCURSES_DIR=$(find . -type d -name "ncurses*" -print -quit)
+cd "$NCURSES_DIR"
 mkdir build && pushd build
 ../configure AWK=gawk
 make -C include
 make -C progs tic
 popd
 ./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess) \
-  --mandir=/usr/share/man --with-manpage-format=normal --with-shared \
-  --without-normal --with-cxx-shared --without-debug --without-ada \
-  --disable-stripping AWK=gawk
+  --mandir=/usr/share/man --with-manpage-format=normal \
+  --with-shared --without-normal --with-cxx-shared \
+  --without-debug --without-ada --disable-stripping AWK=gawk
 make
 make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
 ln -sv libncursesw.so $LFS/usr/lib/libncurses.so
 sed -e 's/^#if.*XOPEN.*$/#if 1/' -i $LFS/usr/include/curses.h
-cd .. && rm -rf ncurses-*/
+cd $LFS/sources
 
-# Bash
-tar -xf bash-*.tar.* && cd bash-*/
-./configure --prefix=/usr --build=$(sh support/config.guess) \
-  --host=$LFS_TGT --without-bash-malloc
-make
-make DESTDIR=$LFS install
-ln -sv bash $LFS/bin/sh
-cd .. && rm -rf bash-*/
+# 6.4 Bash
+build_and_install "bash-" \
+  "./configure --prefix=/usr --build=\$(sh support/config.guess) --host=\$LFS_TGT --without-bash-malloc" "" \
+  "ln -sv bash \$LFS/bin/sh"
 
-# Coreutils
-tar -xf coreutils-*.tar.* && cd coreutils-*/
-FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --host=$LFS_TGT \
-  --build=$(build-aux/config.guess) --enable-install-program=hostname \
-  --enable-no-install-program=kill,uptime
-make
-make DESTDIR=$LFS install
-mv -v $LFS/usr/bin/chroot $LFS/usr/sbin
-mkdir -pv $LFS/usr/share/man/man8
-mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
-sed -i 's/"1"/"8"/' $LFS/usr/share/man/man8/chroot.8
-cd .. && rm -rf coreutils-*/
+# 6.5 Coreutils
+build_and_install "coreutils-" \
+  "FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --host=\$LFS_TGT --build=\$(build-aux/config.guess) --enable-install-program=hostname --enable-no-install-program=kill,uptime" "" \
+  "mv -v \$LFS/usr/bin/chroot \$LFS/usr/sbin && mkdir -pv \$LFS/usr/share/man/man8 && mv -v \$LFS/usr/share/man/man1/chroot.1 \$LFS/usr/share/man/man8/chroot.8 && sed -i 's/\"1\"/\"8\"/' \$LFS/usr/share/man/man8/chroot.8"
 
-# Diffutils
-tar -xf diffutils-*.tar.* && cd diffutils-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(./build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf diffutils-*/
+# 6.6 Diffutils
+build_and_install "diffutils-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(./build-aux/config.guess)" "" ""
 
-# File
-tar -xf file-*.tar.* && cd file-*/
+# 6.7 File
+FILE_DIR=$(find . -type d -name "file-"* -print -quit)
+cd "$FILE_DIR"
 mkdir build && pushd build
 ../configure --disable-bzlib --disable-libseccomp --disable-xzlib --disable-zlib
 make
@@ -86,113 +99,52 @@ popd
 make FILE_COMPILE=$(pwd)/build/src/file
 make DESTDIR=$LFS install
 rm -v $LFS/usr/lib/libmagic.la
-cd .. && rm -rf file-*/
+cd $LFS/sources
 
-# Findutils
-tar -xf findutils-*.tar.* && cd findutils-*/
-./configure --prefix=/usr --localstatedir=/var/lib/locate \
-  --host=$LFS_TGT --build=$(build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf findutils-*/
+# 6.8 Findutils
+build_and_install "findutils-" \
+  "./configure --prefix=/usr --localstatedir=/var/lib/locate --host=\$LFS_TGT --build=\$(build-aux/config.guess)" "" ""
 
-# Gawk
-tar -xf gawk-*.tar.* && cd gawk-*/
+# 6.9 Gawk
+GAWK_DIR=$(find . -type d -name "gawk-"* -print -quit)
+cd "$GAWK_DIR"
 sed -i 's/extras//' Makefile.in
 ./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
 make
 make DESTDIR=$LFS install
-cd .. && rm -rf gawk-*/
+cd $LFS/sources
 
-# Grep
-tar -xf grep-*.tar.* && cd grep-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(./build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf grep-*/
+# 6.10 Grep
+build_and_install "grep-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(./build-aux/config.guess)" "" ""
 
-# Gzip
-tar -xf gzip-*.tar.* && cd gzip-*/
-./configure --prefix=/usr --host=$LFS_TGT
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf gzip-*/
+# 6.11 Gzip
+build_and_install "gzip-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT" "" ""
 
-# Make
-tar -xf make-*.tar.* && cd make-*/
-./configure --prefix=/usr --without-guile --host=$LFS_TGT --build=$(build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf make-*/
+# 6.12 Make
+build_and_install "make-" \
+  "./configure --prefix=/usr --without-guile --host=\$LFS_TGT --build=\$(build-aux/config.guess)" "" ""
 
-# Patch
-tar -xf patch-*.tar.* && cd patch-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf patch-*/
+# 6.13 Patch
+build_and_install "patch-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(build-aux/config.guess)" "" ""
 
-# Sed
-tar -xf sed-*.tar.* && cd sed-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(./build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf sed-*/
+# 6.14 Sed
+build_and_install "sed-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(./build-aux/config.guess)" "" ""
 
-# Tar
-tar -xf tar-*.tar.* && cd tar-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
-make
-make DESTDIR=$LFS install
-cd .. && rm -rf tar-*/
+# 6.15 Tar
+build_and_install "tar-" \
+  "./configure --prefix=/usr --host=\$LFS_TGT --build=\$(build-aux/config.guess)" "" ""
 
-# Xz
-tar -xf xz-*.tar.* && cd xz-*/
-./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess) \
-  --disable-static --docdir=/usr/share/doc/xz-5.6.4
+# 6.16 Xz
+XZ_DIR=$(find . -type d -name "xz-"* -print -quit)
+cd "$XZ_DIR"
+./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess) --disable-static --docdir=/usr/share/doc/xz-5.6.4
 make
 make DESTDIR=$LFS install
 rm -v $LFS/usr/lib/liblzma.la
-cd .. && rm -rf xz-*/
+cd $LFS/sources
 
-# Binutils (pass 2)
-tar -xf binutils-*.tar.* && cd binutils-*/
-sed '6031s/$add_dir//' -i ltmain.sh
-mkdir build && cd build
-../configure --prefix=/usr --build=$(../config.guess) --host=$LFS_TGT \
-  --disable-nls --enable-shared --enable-gprofng=no --disable-werror \
-  --enable-64-bit-bfd --enable-new-dtags --enable-default-hash-style=gnu
-make
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
-cd ../.. && rm -rf binutils-*/
-
-# GCC (pass 2)
-tar -xf gcc-*.tar.* && cd gcc-*/
-tar -xf ../mpfr-*.tar.* && mv -v mpfr-* mpfr
-tar -xf ../gmp-*.tar.* && mv -v gmp-* gmp
-tar -xf ../mpc-*.tar.* && mv -v mpc-* mpc
-
-case $(uname -m) in
-  x86_64)
-    sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
-  ;;
-esac
-
-sed '/thread_header =/s/@.*@/gthr-posix.h/' \
-  -i libgcc/Makefile.in libstdc++-v3/include/Makefile.in
-
-mkdir build && cd build
-../configure --build=$(../config.guess) --host=$LFS_TGT --target=$LFS_TGT \
-  LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc \
-  --prefix=/usr --with-build-sysroot=$LFS --enable-default-pie \
-  --enable-default-ssp --disable-nls --disable-multilib \
-  --disable-libatomic --disable-libgomp --disable-libquadmath \
-  --disable-libsanitizer --disable-libssp --disable-libvtv \
-  --enable-languages=c,c++
-make
-make DESTDIR=$LFS install
-ln -sv gcc $LFS/usr/bin/cc
-cd ../.. && rm -rf gcc-*/
-
-echo "✅ Temporary tools built successfully!"
+echo "🎉 All temporary tools built successfully!"
