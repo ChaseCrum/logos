ef964cffb7b91bd067adae7795c8cf8fb771d621
Chase <chase@unknownde00a889b074.attlocal.net>
2025-07-04 12:27:17 -0500

    fixing sanity check

--- toolchain5.sh
+++ toolchain5.sh
@@ -57,8 +57,9 @@ make DESTDIR=\$LFS install
 # Fix ldd path
 sed '/RTLDLIST=/s@/usr@@g' -i \$LFS/usr/bin/ldd
 
-# Sanity check
-cd \$LFS
+# Sanity check in a writable subdirectory
+mkdir -p \$LFS/tmp/glibc-check
+cd \$LFS/tmp/glibc-check
 echo 'int main(){}' | \$LFS_TGT-gcc -xc -
 readelf -l a.out | grep ld-linux || echo "⚠️ Toolchain test failed"
 rm -v a.out
